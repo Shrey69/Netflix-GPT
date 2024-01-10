6233c393a8761816c45732d3d7bc5b6d68579bf6
Shrey69 <agarwalshrey78@gmail.com>
2024-01-11 01:35:58 +0530

    Env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+//require("dotenv").config( { path : "./"} );
+
 export const LOGO = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
 
 export const PHOTO_URL = 'https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_large.jpg'
@@ -18,4 +20,4 @@ export const API_OPTIONS = {
                             {identifier: "hindi", name: "Hindi"},
                             {identifier: "spanish", name: "Spanish"}];
 
-  export const OPEN_AI_KEY = "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"                            
+  export const OPEN_AI_KEY = process.env.REACT_APP_OPENAI_KEY                  
