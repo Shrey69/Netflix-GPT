74c05e793be7ec3b19bf09b5b59f8c99a0125db1
Shrey69 <agarwalshrey78@gmail.com>
2024-01-20 13:52:15 +0530

    updates

--- src/components/VideoBG.js
+++ src/components/VideoBG.js
@@ -14,7 +14,7 @@ const VideoBG = ({movieId}) => {
       <iframe 
       className='w-screen aspect-video'
       //'https://youtu.be/'
-      src="https://www.youtube.com/embed/OAZWXUkrjPc?si=qsI-yO4uPWf1w8Kb?&autoplay=1&mute=1"
+      src="https://www.youtube.com/embed/m2L-Sa_6MU0?si=kvg8h3Rgpu4oIGMR?&autoplay=1&mute=1"
       title="YouTube video player" 
       allow="accelerometer; autoplay; clipboard-write; encryptd-media; gyroscope; picture-in-picture; web-share" 
      >
